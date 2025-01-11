53182f478885d5d8512b210075e2e377521aef33
Paugdev <paulina.gonzalezx@gmail.com>
2025-01-10 22:26:36 -0300

    se corrigen ambas rutas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 function cambia_imagen(element) {
-    element.src = "..\assets\img\ilustra.jpg"; 
+    element.src = "../img/ilustra.jpg";
 }
 
 function regresa_imagen(element) {
-    element.src = "..\assets\img\pauimg.jpg"; 
+    element.src = "../img/pauimg.jpg"; 
 }
 
 
